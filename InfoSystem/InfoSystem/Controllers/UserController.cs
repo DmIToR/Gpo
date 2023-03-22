2fa7d3c5e23626976f828e5504f8ba7bd8f273a2
Dorafanboy <73462832+Dorafanboy@users.noreply.github.com>
2023-03-22 21:07:34 +0700

    Сделал этому лоху бековую часть

--- InfoSystem/InfoSystem/Controllers/UserController.cs
+++ InfoSystem/InfoSystem/Controllers/UserController.cs
@@ -15,7 +15,58 @@ public class UserController : Controller
         _context = context;
     }
 
-    [HttpGet(Name = "GetUsers")]
+    [HttpGet]
+    [Route("GetUsers")]
     public List<User> GetUsers()
         => _context.Users.ToList();
+
+    [HttpPost]
+    [Route("AddUser")]
+    public Result AddUser(string username, string password)
+    {
+        var user = new User
+        {
+            UserId = Guid.NewGuid(),
+            Login = username,
+            Password = password
+        };
+
+        if (GetUser(username, password) is not null)
+        {
+            return new Result(false, "Такое пользователь существует");
+        }
+
+        _context.Add(user);
+        _context.SaveChanges();
+
+        return new Result(true, "Пользователь добавлен");
+    }
+
+    [HttpGet]
+    [Route("AuthorizeUser")]
+    public Result AuthorizeUser(string username, string password)
+    {
+        var user = GetUser(username, password);
+
+        return user is not null ? new Result(true, $"Пользователь {user.Login} авторизовался") 
+            : new Result(false, "Неправильно введыны данные");
+    }
+
+    private User? GetUser(string username, string password)
+    {
+        return _context.Users
+            .FirstOrDefault(user => user.Login == username && user.Password == password);
+    }
+}
+
+public struct Result
+{
+    public bool IsContains { get; }
+    public string Message { get; }
+
+    public Result(bool isContains, string message)
+    {
+        IsContains = isContains;
+        Message = message;
+    }
 }
